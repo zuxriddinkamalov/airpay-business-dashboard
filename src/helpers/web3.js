17c60e5244258af0466da6d3e499ccdf65065221
Misha Yefimets <misha.efimets@gmail.com>
2018-11-27 10:44:56 +0100

    fix get

--- src/helpers/web3.js
+++ src/helpers/web3.js
@@ -1,7 +1,8 @@
 import Web3 from 'web3';
 let web3;
-if (window.ethereum) {
-  web3 = new Web3(ethereum);
+if (typeof window !== 'undefined' && typeof window.web3 !== 'undefined') {
+  // We are in the browser and metamask is running.
+  web3 = new Web3(window.web3.currentProvider);
 }
 
 export default web3;
