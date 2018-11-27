b1942d8aa2f5564f0f50212b4b14766598e0cf99
Misha Yefimets <misha.efimets@gmail.com>
2018-11-27 10:40:43 +0100

    deplo

--- server.js
+++ server.js
@@ -3,7 +3,10 @@
 var express = require('express');
 var path = require('path');
 var serveStatic = require('serve-static');
+var history = require('connect-history-api-fallback');
+
 app = express();
+app.use(history({ verbose: true }));
 app.use(serveStatic('dist/'));
 var port = process.env.PORT || 5000;
 app.listen(port);
