1359441eca0997d4d113cb962994b89c9047e6d8
Misha Yefimets <misha.efimets@gmail.com>
2018-12-03 20:00:48 +0200

    fix env

--- src/constant/general.js
+++ src/constant/general.js
@@ -1,3 +1,3 @@
 export const LOGO = process.env.VUE_APP_LOGO;
-export const MAIN_COLOR = process.env.MAIN_COLOR;
-export const NAME = process.env.NAME;
+export const MAIN_COLOR = process.env.VUE_APP_MAIN_COLOR;
+export const NAME = process.env.VUE_APP_NAME;
--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,14 @@ module.exports = {
   css: {
     loaderOptions: {
       sass: {
-        data: '$BASE_COLOR: ' + process.env.BASE_COLOR + ';'
+        data:
+          '$--color-primary: ' +
+          process.env.VUE_APP_MAIN_COLOR +
+          '; $BG_COLOR: ' +
+          process.env.VUE_APP_BG_COLOR +
+          '; $BASE_COLOR: ' +
+          process.env.VUE_APP_BASE_COLOR +
+          ';'
       }
     }
   }
