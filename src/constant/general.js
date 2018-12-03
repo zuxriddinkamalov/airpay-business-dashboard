676c141195d5d86147a42dee033dc2e615495b2d
Misha Yefimets <misha.efimets@gmail.com>
2018-12-03 20:19:41 +0200

    fix ens

--- src/constant/general.js
+++ src/constant/general.js
@@ -1,3 +1,9 @@
-export const LOGO = process.env.VUE_APP_LOGO;
-export const MAIN_COLOR = process.env.VUE_APP_MAIN_COLOR;
-export const NAME = process.env.VUE_APP_NAME;
+export const LOGO = process.env.VUE_APP_LOGO
+  ? process.env.VUE_APP_LOGO
+  : 'https://s3.us-east-2.amazonaws.com/airpay-network/DIP_logo.png';
+export const MAIN_COLOR = process.env.VUE_APP_MAIN_COLOR
+  ? process.env.VUE_APP_MAIN_COLOR
+  : '#FFC408';
+export const NAME = process.env.VUE_APP_NAME
+  ? process.env.VUE_APP_NAME
+  : 'NAME';
--- vue.config.js
+++ vue.config.js
@@ -3,13 +3,13 @@ module.exports = {
     loaderOptions: {
       sass: {
         data:
-          '$--color-primary: ' +
-          process.env.VUE_APP_MAIN_COLOR +
-          '; $BG_COLOR: ' +
-          process.env.VUE_APP_BG_COLOR +
-          '; $BASE_COLOR: ' +
-          process.env.VUE_APP_BASE_COLOR +
-          ';'
+          '$--color-primary: ' + process.env.VUE_APP_MAIN_COLOR
+            ? process.env.VUE_APP_MAIN_COLOR
+            : '#FFC408' + '; $BG_COLOR: ' + process.env.VUE_APP_BG_COLOR
+              ? process.env.VUE_APP_BG_COLOR
+              : '#F6F6F6' + '; $BASE_COLOR: ' + process.env.VUE_APP_BASE_COLOR
+                ? process.env.VUE_APP_BASE_COLOR
+                : '#565656' + ';'
       }
     }
   }
